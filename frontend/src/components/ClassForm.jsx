ac9ae53798ff9b36cc536f650372b502b846f188
guiroos <guilherme.roosr@gmail.com>
2022-06-28 15:48:47 -0300

    feat: created ClassForm component

--- frontend/src/components/ClassForm.jsx
+++ frontend/src/components/ClassForm.jsx
@@ -1,7 +1,92 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { apiPostBody, apiPut } from "../utils/axios";
 
-function ClassForm() {
-  return <div>CreateClassForm</div>;
+export default function ClassForm({
+  handleIsEditing,
+  buttonClicked,
+  route,
+  axiosApi,
+}) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      className: "",
+      year: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    if (axiosApi === "post") {
+      try {
+        apiPostBody("/classes", data);
+        buttonClicked();
+      } catch (error) {
+        console.log(error);
+        buttonClicked();
+      }
+    }
+    if (axiosApi === "put") {
+      try {
+        apiPut(route, data);
+        buttonClicked();
+      } catch (error) {
+        console.log(error);
+        buttonClicked();
+      }
+    }
+    buttonClicked();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            id="className"
+            placeholder="Class name"
+            {...register("className", {
+              required: true,
+              minLength: {
+                value: 2,
+                message: "Name must have more than one character",
+              },
+            })}
+          />
+          {errors.className && (
+            <p id="className-error">{errors.className.message}</p>
+          )}
+        </div>
+
+        <div>
+          <input
+            type="number"
+            id="year"
+            placeholder="Year"
+            {...register("year", {
+              required: true,
+              minLength: {
+                value: 2,
+                message: "Must have two digits",
+              },
+            })}
+          />
+          {errors.year && <p id="year-error">{errors.year.message}</p>}
+        </div>
+
+        <div>
+          <button type="reset" onClick={() => handleIsEditing()}>
+            Cancel
+          </button>
+          <button type="submit" disabled={!isValid}>
+            {axiosApi === "post" ? "Create" : "Update"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
-
-export default ClassForm;

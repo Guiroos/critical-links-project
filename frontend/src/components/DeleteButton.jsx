5be24eaafc98885bd50973f7373f3844c96dfab1
guiroos <guilherme.roosr@gmail.com>
2022-06-29 00:42:24 -0300

    style: tailwind for DeleteButton component

--- frontend/src/components/DeleteButton.jsx
+++ frontend/src/components/DeleteButton.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { apiDelete } from "../utils/axios";
 
-export default function DeleteButton({
-  handleShow,
-  route,
-  buttonClicked,
-}) {
+export default function DeleteButton({ handleShow, route, buttonClicked }) {
   return (
-    <div>
-      <div>Are you sure you want to delete?</div>
-      <div>
+    <div className="flex flex-col mt-4">
+      <p>Are you sure you want to delete?</p>
+      <div className="flex gap-4 justify-end text-[#3F51B5]">
         <button onClick={() => handleShow()}>NO</button>
         <button
           onClick={() => {

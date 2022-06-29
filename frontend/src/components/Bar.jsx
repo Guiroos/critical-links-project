5ab5c80ec642488ef38f8d4c1a5a67106d5cf8ed
guiroos <guilherme.roosr@gmail.com>
2022-06-29 00:51:27 -0300

    style: tailwind for Bar component

--- frontend/src/components/Bar.jsx
+++ frontend/src/components/Bar.jsx
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import StudentForm from "./StudentForm";
 import ClassForm from "./ClassForm";
 import ManageClasses from "./ManageClasses";
+import OnClickButton from "./OnClickButton";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-export default function Bar({
-  classes,
-  classesOptions,
-  buttonClicked,
-}) {
+export default function Bar({ classes, classesOptions, buttonClicked }) {
   const [isStudentForm, setIsStudentForm] = useState(false);
   const [isClassForm, setIsClassForm] = useState(false);
   const [isManageClasses, setIsManageClasses] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   const handleStudentFormClick = () => {
     setIsStudentForm(!isStudentForm);
@@ -30,21 +30,25 @@ export default function Bar({
     setIsClassForm(false);
   };
 
+  const handleMobile = () => {
+    setIsManageClasses(false);
+    setIsStudentForm(false);
+    setIsClassForm(false);
+    setIsMobile(!isMobile);
+  };
+
   return (
-    <div>
-      <div>
-        <h1>Student Manager</h1>
+    <div className="flex justify-between mx-8 my-4">
+      <div className="text-3xl text-[#5B5B5B]">
+        <p>Student Manager</p>
       </div>
-      <div>
-        <button onClick={() => setIsStudentForm(!isStudentForm)}>
-          Create Student
-        </button>
-        <button onClick={() => setIsClassForm(!isClassForm)}>
-          Create Class
-        </button>
-        <button onClick={() => setIsManageClasses(!isManageClasses)}>
-          Manage Classes
-        </button>
+      <div className="hidden desktop:flex desktop:gap-3">
+        <OnClickButton content="Create Student" func={handleStudentFormClick} />
+        <OnClickButton content="Create Class" func={handleClassFormClick} />
+        <OnClickButton
+          content="Manage Classes"
+          func={handleManageClassesClick}
+        />
         {isStudentForm && (
           <StudentForm
             classesOptions={classesOptions}
@@ -67,6 +71,71 @@ export default function Bar({
             buttonClicked={buttonClicked}
           />
         )}
+      </div>
+
+      <div className="desktop:hidden">
+        <OnClickButton
+          content={<FaBars size={32} />}
+          icon={"bar"}
+          func={handleMobile}
+        />
+        <div
+          className={
+            !isMobile
+              ? "hidden"
+              : " absolute top-0 left-0 py-4 px-8 w-full h-screen flex flex-col items-center bg-white"
+          }
+        >
+          <div className="flex justify-end w-full">
+            <OnClickButton
+              content={<FaTimes size={32} />}
+              icon={"bar"}
+              func={handleMobile}
+            />
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col py-8 gap-4 h-full text-3xl font-bold cursor-pointer">
+              <div className="border-b-2 w-full p-2">
+                <p className="w-fit p-3" onClick={handleClassFormClick}>
+                  Create Class
+                </p>
+              </div>
+              <div className="border-b-2 w-full p-2">
+                <p className="w-fit p-3" onClick={handleStudentFormClick}>
+                  Create student
+                </p>
+              </div>
+              <div className="border-b-2 w-full p-2">
+                <p className="w-fit p-3" onClick={handleManageClassesClick}>
+                  Manage classes
+                </p>
+              </div>
+            </div>
+            {isStudentForm && (
+              <StudentForm
+                classesOptions={classesOptions}
+                handleStudentFormClick={handleStudentFormClick}
+                buttonClicked={buttonClicked}
+                axiosApi="post"
+              />
+            )}
+            {isClassForm && (
+              <ClassForm
+                handleClassFormClick={handleClassFormClick}
+                buttonClicked={buttonClicked}
+                axiosApi="post"
+              />
+            )}
+
+            {isManageClasses && (
+              <ManageClasses
+                classes={classes}
+                handleManageClassesClick={handleManageClassesClick}
+                buttonClicked={buttonClicked}
+              />
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );

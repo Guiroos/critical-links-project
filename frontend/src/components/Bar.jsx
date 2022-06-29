b1c296bd243a8131438fd1667bd0bfedd5dac107
guiroos <guilherme.roosr@gmail.com>
2022-06-29 19:11:14 -0300

    feat: add popup window when button click and ids

--- frontend/src/components/Bar.jsx
+++ frontend/src/components/Bar.jsx
@@ -38,20 +38,30 @@ export default function Bar({ classes, classesOptions, buttonClicked }) {
   };
 
   return (
-    <div className="flex justify-between mx-8 my-4">
+    <div id="navbar" className="flex justify-between mx-8 my-4">
       <div className="text-3xl text-[#5B5B5B]">
-        <p>Student Manager</p>
+        <p id="navbar-title">Student Manager</p>
       </div>
       <div className="hidden desktop:flex desktop:gap-3">
-        <OnClickButton content="Create Student" func={handleStudentFormClick} />
-        <OnClickButton content="Create Class" func={handleClassFormClick} />
         <OnClickButton
+          id="navbar-create-student-button"
+          content="Create Student"
+          func={handleStudentFormClick}
+        />
+        <OnClickButton
+          id="navbar-create-class-button"
+          content="Create Class"
+          func={handleClassFormClick}
+        />
+        <OnClickButton
+          id="navbar-manage-classes-button"
           content="Manage Classes"
           func={handleManageClassesClick}
         />
         {isStudentForm && (
           <div className="absolute top-0 left-0 py-4 px-8 w-full h-screen flex flex-col justify-center items-center">
             <button
+              id="navbar-close-student-form-button"
               type="button"
               tabIndex={-1}
               onClick={handleStudentFormClick}
@@ -70,6 +80,7 @@ export default function Bar({ classes, classesOptions, buttonClicked }) {
         {isClassForm && (
           <div className="absolute top-0 left-0 py-4 px-8 w-full h-screen flex flex-col justify-center items-center">
             <button
+              id="navbar-close-class-form-button"
               type="button"
               tabIndex={-1}
               onClick={handleClassFormClick}
@@ -85,16 +96,28 @@ export default function Bar({ classes, classesOptions, buttonClicked }) {
           </div>
         )}
         {isManageClasses && (
-          <ManageClasses
-            classes={classes}
-            handleManageClassesClick={handleManageClassesClick}
-            buttonClicked={buttonClicked}
-          />
+          <div className="absolute top-0 left-0 py-4 px-8 w-full h-screen flex flex-col justify-center items-center">
+            <button
+              id="navbar-close-manage-classes-button"
+              type="button"
+              tabIndex={-1}
+              onClick={handleManageClassesClick}
+              className="fixed inset-0 w-screen h-screen cursor-default"
+            />
+            <div className="border p-4 shadow-lg bg-white z-10">
+              <ManageClasses
+                classes={classes}
+                handleManageClassesClick={handleManageClassesClick}
+                buttonClicked={buttonClicked}
+              />
+            </div>
+          </div>
         )}
       </div>
 
       <div className="desktop:hidden">
         <OnClickButton
+          id="navbar-icon-open-button"
           content={<FaBars size={32} />}
           icon={"bar"}
           func={handleMobile}
@@ -108,6 +131,7 @@ export default function Bar({ classes, classesOptions, buttonClicked }) {
         >
           <div className="flex justify-end w-full">
             <OnClickButton
+              id="navbar-icon-close-button"
               content={<FaTimes size={32} />}
               icon={"bar"}
               func={handleMobile}
@@ -115,44 +139,57 @@ export default function Bar({ classes, classesOptions, buttonClicked }) {
           </div>
           <div className="flex flex-col">
             <div className="flex flex-col py-8 gap-4 h-full text-3xl font-bold cursor-pointer">
-              <div className="border-b-2 w-full p-2">
-                <p className="w-fit p-3" onClick={handleClassFormClick}>
-                  Create Class
-                </p>
+              <div
+                id="navbar-mobile-create-class-button"
+                className="border-b-2 w-full p-2"
+                onClick={handleClassFormClick}
+              >
+                <p className="w-fit p-3">Create Class</p>
               </div>
-              <div className="border-b-2 w-full p-2">
-                <p className="w-fit p-3" onClick={handleStudentFormClick}>
-                  Create student
-                </p>
+              <div
+                id="navbar-mobile-create-student-button"
+                className="border-b-2 w-full p-2"
+                onClick={handleStudentFormClick}
+              >
+                <p className="w-fit p-3">Create student</p>
               </div>
-              <div className="border-b-2 w-full p-2">
-                <p className="w-fit p-3" onClick={handleManageClassesClick}>
-                  Manage classes
-                </p>
+              <div
+                id="navbar-mobile-manage-classes-button"
+                className="border-b-2 w-full p-2"
+                onClick={handleManageClassesClick}
+              >
+                <p className="w-fit p-3">Manage classes</p>
               </div>
             </div>
             {isStudentForm && (
-              <StudentForm
-                classesOptions={classesOptions}
-                handleStudentFormClick={handleStudentFormClick}
-                buttonClicked={buttonClicked}
-                axiosApi="post"
-              />
+              <div id="mobile-student-form">
+                <StudentForm
+                  classesOptions={classesOptions}
+                  handleStudentFormClick={handleStudentFormClick}
+                  buttonClicked={buttonClicked}
+                  axiosApi="post"
+                />
+              </div>
             )}
+
             {isClassForm && (
-              <ClassForm
-                handleClassFormClick={handleClassFormClick}
-                buttonClicked={buttonClicked}
-                axiosApi="post"
-              />
+              <div id="mobile-class-form">
+                <ClassForm
+                  handleClassFormClick={handleClassFormClick}
+                  buttonClicked={buttonClicked}
+                  axiosApi="post"
+                />
+              </div>
             )}
 
             {isManageClasses && (
-              <ManageClasses
-                classes={classes}
-                handleManageClassesClick={handleManageClassesClick}
-                buttonClicked={buttonClicked}
-              />
+              <div id="mobile-manage-classes">
+                <ManageClasses
+                  classes={classes}
+                  handleManageClassesClick={handleManageClassesClick}
+                  buttonClicked={buttonClicked}
+                />
+              </div>
             )}
           </div>
         </div>
